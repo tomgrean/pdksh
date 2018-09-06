846170e2d34dcc13e71e8aeb824428e79e949a42
xiaowp <xiaowp@qq.com>
2018-09-06 15:00:17 +0800

    use native c version instead of shell script. the shell script rely on the exact text output.

---
+++ siglist.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sh.h"
+
+struct xsiginfo {
+	int x_signal;
+	char x_name[32];
+	char x_mess[128];
+};
+
+
+static struct xsiginfo xsigs[] = {
+#include SIGNALIN
+
+	{-1, "",""}
+};
+
+int main()
+{
+	int i, n;
+	int max_signal = 0;
+
+	for (i = 0; xsigs[i].x_signal > 0; i++) {
+		if (xsigs[i].x_signal > max_signal) {
+			max_signal = xsigs[i].x_signal;
+		}
+	}
+	for (n = 1; n < max_signal; n++) {//omit the biggest 'signal'.
+		int not_found = 1;
+		for (i = 0; xsigs[i].x_signal > 0; i++) {
+			if (xsigs[i].x_signal == n) {
+				printf("{.signal=%d,.name=\"%s\",.mess=\"%s\"},\n", n, xsigs[i].x_name, xsigs[i].x_mess);
+				not_found = 0;
+				break;
+			}
+		}
+		if (not_found) {
+			printf("{.signal=%d,.name=(char *)0,.mess=\"Signal %d\"},\n", n, n);
+		}
+	}
+	return 0;
+}
+
--- siglist.sh
+++ siglist.sh
@@ -11,35 +11,17 @@ in=tmpi$$.c
 out=tmpo$$.c
 ecode=1
 trapsigs='0 1 2 13 15'
-trap 'rm -f $in $out; trap 0; exit $ecode' $trapsigs
+trap 'rm -f $in $out siglist; trap 0; exit $ecode' $trapsigs
 
-CPP="${1-cc -E}"
+CC="${1-cc}"
 
 # The trap here to make up for a bug in bash (1.14.3(1)) that calls the trap
 (trap - $trapsigs;
- echo '#include "sh.h"';
- echo '	{ QwErTy SIGNALS , "DUMMY" , "hook for number of signals" },\n';
+ echo '	{ SIGNALS , "DUMMY" , "hook for number of signals" },';
+ echo 
  sed -e '/^[	 ]*#/d' -e 's/^[	 ]*\([^ 	][^ 	]*\)[	 ][	 ]*\(.*[^ 	]\)[ 	]*$/#ifdef SIG\1\
-	{ QwErTy SIG\1 , "\1", "\2" },\
+	{ SIG\1 , "\1", "\2" },\
 #endif/') > $in
-$CPP $in  > $out
-
-awk '/{ QwErTy.*}/{print;next;}
-/{ QwErTy/{a=1;printf "{";next}
-a>0 && !/^#/ {
-  printf $0;
-  if($NF=="},"){printf "\n";a=0;}
-}' $out | sed 's/ QwErTy//g' |
-sort -k 2n |
-awk 'BEGIN { last=0; } {
-if ($2 ~ /^[0-9][0-9]*$/ && $3 == ",") {
-  n = $2;
-  if (n > 0 && n != last) {
-    while (++last < n) {
-      printf "{ %d , (char *) 0, \"Signal %d\" },\n", last, last;
-    }
-    print;
-  }
-}
-}' | awk -F, '/DUMMY/{next;}{sig=$1;sub(/[ \t]*{/,"",sig);print "{.signal= " sig " ,.name= " $2 " ,.mess= " $3 ","}'
+$CC -DSIGNALIN="\"$in\"" siglist.c -o siglist
+./siglist
 ecode=0
